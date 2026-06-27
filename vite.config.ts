f4e16f42888ff62c6271f1b7dbf02e8cfd388072
Fredi Bach <osxcode@gmail.com>
2026-06-27 16:49:35 +0200

    Add bundle analyser

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [tailwindcss(), react(), process.env.ANALYZE ? visualizer({ open: true, gzipSize: true, brotliSize: true }) : null],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
